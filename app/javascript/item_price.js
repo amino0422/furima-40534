6d8a8b9bb93160feecbc2861d49a41e1521e6f51
amino0422 <sekainoowari.nakajin@gmail.com>
2024-05-02 12:30:21 +0900

    販売手数料・販売利益の表示の修正３

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+window.addEventListener('turbo:load', () => {
 
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
@@ -7,7 +7,7 @@ window.addEventListener('load', () => {
       const addTaxDom = document.getElementById("add-tax-price");
       addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
 
-      const profitDom = document.getElementById("profit");
-      profitDom.innerHTML = Math.floor(inputValue * 0.9);
+      const addProfitDom = document.getElementById("profit");
+      addProfitDom.innerHTML = Math.floor(inputValue * 0.9);
   });
 });
